6f8d2209302a5f2827f52236498bdb25b0522dfc
Space-Pigeon <chrism.patmore@btinternet.com>
2017-01-25 16:11:21 +0000

    Update TestNet.py

--- TestNet.py
+++ TestNet.py
@@ -1,6 +1,10 @@
 import Training as Tr
 
+#This file will train the network for either the same GridWorld initial setup (0) or for a game with a random initial player position(1)
+#Can do with all random(2) if someone wants to write the Q-Learing code in Training.
 Tr.trainNet(1)
+
+#Test the trained net.
 Tr.testAlgo(1)
 Tr.testAlgo(1)
 Tr.testAlgo(1)
