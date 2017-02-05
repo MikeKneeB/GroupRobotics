7fdecd8b089e6bde9f75bb9fb0fae57724ca0840
Shaw <has315@student.bham.ac.uk>
2017-02-05 22:53:52 +0000

    Some more actor critic skeleton

--- MachineLearning/shawac/actorcritic.py
+++ MachineLearning/shawac/actorcritic.py
@@ -6,21 +6,30 @@ class ActorCritic():
         self.actor = Actor(stateDimensionsList, numberOfActions)
         self.critic = Critic(stateDimensionsList, timeHorizon)
 
-
+    def critique(self, state):
+        tDError = self.critic.getTDError(state, self.actor.policy)
+        self.actor.updatePolicy(state, tDError)
 
     def getNextAction(self, state):
-        error = self.critic.getTDError(state, self.actor.policy)
-
+        actions = self.actor.policy[state]
+        nextAction = actions.argmax(actions) ##get the action with the largest TD thing???
+        assert 0 <= nextAction < self.numberOfActions
+        return nextAction
 
 class Actor():
     def __init__(self, stateDimensions, numberOfActions):
         policyDimensions = stateDimensions + (numberOfActions,)
         self.policy = np.zeros(policyDimensions)
 
-    def updatePolicy(self):
+    def updatePolicy(self, state, tDError):
+        ##something in here
         pass
 
+
 class Critic():
+    def __init__(self, stateDimensionsList, timeHorizon):
+        pass
+
     def getTDError(self, state, policy):
         return 0
 
