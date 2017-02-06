74fd1d8b3e77c2d28c370e8fb4023e47d865b315
Shaw <has315@student.bham.ac.uk>
2017-02-06 22:52:58 +0000

    Some more actor critic skeleton Hit a snag. Basic actor-critic only works when action-state relation known. Have to fiddle. Currently policy structure has a value for every state-action. Could instead have an object with value and the next state.

--- MachineLearning/shawac/actorcritic.py
+++ MachineLearning/shawac/actorcritic.py
@@ -1,36 +1,49 @@
 import numpy as np
-class ActorCritic():
 
-    def __init__(self, stateDimensionsList, numberOfActions, timeHorizon):
+# Call get next action -> perform that action -> critique with new sate->
+
+class ActorCritic:
+
+    def __init__(self, numberOfActions, timeHorizon, rewards, discount):
         self.numberOfActions = numberOfActions
-        self.actor = Actor(stateDimensionsList, numberOfActions)
-        self.critic = Critic(stateDimensionsList, timeHorizon)
-
-    def critique(self, state):
-        tDError = self.critic.getTDError(state, self.actor.policy)
-        self.actor.updatePolicy(state, tDError)
+        self.actor = Actor(rewards.shape, numberOfActions)
+        self.critic = Critic(timeHorizon, rewards, discount)
 
     def getNextAction(self, state):
-        actions = self.actor.policy[state]
-        nextAction = actions.argmax(actions) ##get the action with the largest TD thing???
-        assert 0 <= nextAction < self.numberOfActions
-        return nextAction
+        return getNextAction(state, self.actor.policy)
 
-class Actor():
-    def __init__(self, stateDimensions, numberOfActions):
-        policyDimensions = stateDimensions + (numberOfActions,)
+    def critique(self, previousState, previousAction, state):
+        tDError = self.critic.getTDError(previousState, state, self.actor.policy)
+        self.actor.updatePolicy(previousState, previousAction, tDError)
+
+class Actor:
+    def __init__(self, stateSpaceDimensions, numberOfActions):
+        policyDimensions = stateSpaceDimensions + (numberOfActions,)
         self.policy = np.zeros(policyDimensions)
 
-    def updatePolicy(self, state, tDError):
-        ##something in here
-        pass
+    def updatePolicy(self, state, action, tDError):
+        self.policy[state + (action,)] += tDError
 
 
-class Critic():
-    def __init__(self, stateDimensionsList, timeHorizon):
-        pass
+class Critic:
+    def __init__(self, timeHorizon, rewards, discount):
+        self.rewards = rewards
+        self.timeHorizon = timeHorizon
+        self.discount = discount
 
-    def getTDError(self, state, policy):
+    def getTDError(self, state, nextState, policy):
         return 0
 
-    pass
+    def value(self, state, policy):
+        value = 0
+        for t in range(self.timeHorizon):
+            reward = self.rewards[state]
+            #calc value
+
+            state = state #what am I doing
+
+
+def getNextAction(state, policy):
+    actions = policy[state]
+    nextAction = np.argmax(actions)
+    return nextAction
