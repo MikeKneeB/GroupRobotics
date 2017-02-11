d8f8d4d96dc4ec06307e8af68947f1a1ba775c7c
Mike Knee <mikeknee@hotmail.co.uk>
2017-02-11 23:23:34 +0000

    Commmmmmmmmmmments.

--- MachineLearning/ActorCritic/ActorCriticTrain.py
+++ MachineLearning/ActorCritic/ActorCriticTrain.py
@@ -10,113 +10,127 @@ import datetime
 import actor
 import critic
 
+"""
+train trains the neural network objects using the tensorflow session sess.
+Can be run with varying parameters, though recommended defaults will be fine
+in many cases. Environments can be any kind of continuous openai env.
+
+sess: tensorflow session to run on.
+actor_model: actor network object.
+critic_model: critic network object.
+env: openai environment.
+epochs: number of training runs to run.
+run_length: number of actions available to the agent in one run.
+batch_size: memory batch for online training.
+gamma: learning rate (set high).
+epsilon: noise range.
+min_epsilon: smallest epsilon to use (reached at final run of training).
+buffer: memory buffer size (number of stored memories).
+filepath: optional filepath to save data too, in most cases not needed.
+render: show environment state during training.
+"""
 def train(sess, actor_model, critic_model, env, epochs = 1000, run_length = 300, batch_size = 40, gamma = 0.95, epsilon = 1, min_epsilon = 0.01, buffer = 1000, filepath=None, render=False):
 
     now = datetime.datetime.now()
-    #actor_model = ACNetworks.ActorNet.create_actor()
-    #critic_model = ACNetworks.CriticNet.create_critic()
 
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
 
     max_action = env.action_space.high
 
-    if filepath is not None: 
-        f = open(filepath, 'w')
+    if filepath is None: 
+        filename = '{}:{}_{}-{}-{}_e{}-l{}_m{}-b{}_ep{}'.format(now.hour, now.minute, now.day, now.month, now.year, epochs, run_length, buffer, batch_size, epsilon)
     else:
-        f = open('{}:{}_{}-{}-{}_e{}-l{}_m{}-b{}'.format(now.hour, now.minute, now.day, now.month, now.year, epochs, run_length, buffer, batch_size), 'w')
-    f.write('{}\t{}\n'.format('ep','re'))
+        filename = filepath
 
-    #if actor_path == None and critic_path == None:
-    #    actor_model = create_actor()
-    #    critic_model = create_critic()
-    #elif actor_path == None or critic_path == None:
-    #    print('If you supply one network, you must supply both.')
-    #    sys.exit(1)
-    #else:
-    #actor_model = actor.ActorNetwork(sess, state_dim, action_dim, max_action, 0.0001, 0.001) 
-    #critic_model = critic.CriticNetwork(sess, state_dim, action_dim, max_action, 0.001, 0.001, actor_model.get_num_trainable_vars()) 
+    with open(filename, 'w') as f:
 
-    sess.run(tf.global_variables_initializer())
+        f.write('{}\t{}\n'.format('ep','re'))
 
-    actor_model.update_target_network()
-    critic_model.update_target_network()
+        sess.run(tf.global_variables_initializer())
 
-    replay = []
+        actor_model.update_target_network()
+        critic_model.update_target_network()
 
-    for epo in range(epochs+1):
-        print('Game: %s' % epo)
+        replay = []
 
-        obs_1 = env.reset()
+        for epo in range(epochs+1):
+            print('Game: %s' % epo)
 
-        reward_total = 0
-        reward = 0
+            obs_1 = env.reset()
 
-        for j in range(run_length):
-            if render:
-                env.render()
+            reward_total = 0
+            reward = 0
 
-            print('Obs: {}'.format(obs_1.reshape(1, state_dim)))
+            for j in range(run_length):
+                if render:
+                    env.render()
 
-            #if (random.random() < epsilon):
-            #    print('Rand action')
-            #    action = [random.uniform(-max_action, max_action)] 
-            #else:
-            #    action = actor_model.predict(obs_1.reshape(1, state_dim)) 
-            #    print('Action: {}'.format(action))
+                print('Obs: {}'.format(obs_1.reshape(1, state_dim)))
 
-            noise_r = epsilon/2.
-            action = actor_model.predict(obs_1.reshape(1, state_dim)) + random.uniform(-noise_r, noise_r)
+                noise_r = epsilon/2.
+                action = actor_model.predict(obs_1.reshape(1, state_dim)) + random.uniform(-noise_r, noise_r)
 
-            print('Act val: {} [{}, {}]'.format(action, epo, j))
+                print('Act val: {} [{}, {}]'.format(action, epo, j))
 
-            obs_2, reward, d, i = env.step(action)
+                obs_2, reward, d, i = env.step(action)
 
-            replay.append([np.reshape(obs_1, state_dim), np.reshape(action, action_dim), reward, np.reshape(obs_2, state_dim)])
+                replay.append([np.reshape(obs_1, state_dim), np.reshape(action, action_dim), reward, np.reshape(obs_2, state_dim)])
 
-            while len(replay) > buffer:
-                replay.pop(0)
+                while len(replay) > buffer:
+                    replay.pop(0)
 
-            if len(replay) >= buffer:
-                #o_batch, a_batch, r_batch, o2_batch = random.sample(replay, batch_size)
-                
-                mem = random.sample(replay, batch_size)
-                o_batch = column(mem, 0)
-                a_batch = column(mem, 1)
-                r_batch = column(mem, 2)
-                o2_batch = column(mem, 3)
+                if len(replay) >= buffer:
+                    mem = random.sample(replay, batch_size)
+                    o_batch = column(mem, 0)
+                    a_batch = column(mem, 1)
+                    r_batch = column(mem, 2)
+                    o2_batch = column(mem, 3)
 
-                q_batch = critic_model.predict_target(o2_batch, actor_model.predict_target(o2_batch))
+                    q_batch = critic_model.predict_target(o2_batch, actor_model.predict_target(o2_batch))
 
-                y_train = []
+                    y_train = []
 
-                for k in range(batch_size):
-                    y_train.append(r_batch[k] + gamma*q_batch[k])
+                    for k in range(batch_size):
+                        y_train.append(r_batch[k] + gamma*q_batch[k])
 
-                predict_q, _ = critic_model.train(o_batch, a_batch, y_train)
+                    predict_q, _ = critic_model.train(o_batch, a_batch, y_train)
 
-                a_out = actor_model.predict(o_batch)
-                a_gradient = critic_model.action_gradients(o_batch, a_out)
-                actor_model.train(o_batch, a_gradient[0])
+                    a_out = actor_model.predict(o_batch)
+                    a_gradient = critic_model.action_gradients(o_batch, a_out)
+                    actor_model.train(o_batch, a_gradient[0])
 
-                actor_model.update_target_network()
-                critic_model.update_target_network()
+                    actor_model.update_target_network()
+                    critic_model.update_target_network()
 
-            obs_1 = obs_2[:]
+                obs_1 = obs_2[:]
 
-            if epsilon > min_epsilon:
-                epsilon -= (1.0/(epochs*run_length))
+                if epsilon > min_epsilon:
+                    epsilon -= (1.0/(epochs*run_length))
+                    if epsilon < min_epsilon:
+                        epsilon = min_epsilon
 
-            reward_total += reward
+                reward_total += reward
 
-            if d:
-                continue
+                if d:
+                    continue
 
-        print('Reward earned: {}'.format(reward_total))
-        f.write('{}\t{}\n'.format(epo, reward_total[0]))
+            print('Reward earned: {}'.format(reward_total))
+            f.write('{}\t{}\n'.format(epo, reward_total[0]))
 
-    f.close()
+        f.close()
 
+"""
+Test tests a trained agent (both nets) on an environment. This does not produce  
+data and always renders.
+
+sess: tensorflow learn session.
+actor_model: actor object (should already be trained).
+critic_model: critic object (should also already be trained).
+env: openai environment.
+epochs: number of test runs.
+run_length: number of actions available to the agent in one run.
+"""
 def test(sess, actor_model, critic_model, env, epochs = 1000, run_length = 300):
     for epo in range(epochs+1):
         print('Game: %s' % epo)
@@ -148,6 +162,12 @@ def test(sess, actor_model, critic_model, env, epochs = 1000, run_length = 300):
  
     return 0
 
+"""
+Returns a column of a list of lists as a single array.
+
+matrix: list of lists to get column.
+i: column index.
+"""
 def column(matrix, i):
     return [row[i] for row in matrix]
 
@@ -162,7 +182,7 @@ if __name__ == '__main__':
         actor_model = actor.ActorNetwork(sess, state_dim, action_dim, max_action, 0.0001, 0.001) 
         critic_model = critic.CriticNetwork(sess, state_dim, action_dim, max_action, 0.001, 0.001, actor_model.get_num_trainable_vars()) 
 
-        train(sess, actor_model, critic_model, env, epochs=200, run_length=300, render=True)
+        train(sess, actor_model, critic_model, env, epochs=500, run_length=300, render=False)
 
         raw_input('Training complete, press enter to continue to test.')
 
