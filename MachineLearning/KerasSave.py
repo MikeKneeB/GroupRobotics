7deb69678ea33650405b596cbbe58715945876dc
Mike Knee <mikeknee@hotmail.co.uk>
2017-02-13 15:34:46 +0000

    Deprecated code moved to archive folder.

--- MachineLearning/KerasSave.py
+++
@@ -1,8 +0,0 @@
-from keras.models import Sequential, load_model 
-
-def SaveModel(model, filename):
-    model.save(filename+'.h5')
-    print "Save complete."
-
-def LoadModel(filename):
-    return load_model(filename+'.h5')
