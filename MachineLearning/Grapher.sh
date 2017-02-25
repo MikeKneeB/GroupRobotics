0811d8425a0b1b1104f41d14e83c30c2f0aa3790
Mike Knee <mikeknee@hotmail.co.uk>
2017-02-25 00:16:03 +0000

    Improvements to the grapher script.

--- MachineLearning/Grapher.sh
+++ MachineLearning/Grapher.sh
@@ -1,23 +1,60 @@
 #!/bin/bash
 
 # Grapher script for gnuplot.
-# To use ensure data file is tab delimited, with headers ep for epochs, re for
+# To use: ensure data file is tab delimited, with headers ep for epochs, re for
 # reward, rol_re for rolling reward and epsilon for noise factor. Noise factor
 # column can safely be left as zeroes or all blank.
 
 # Ask Mike for help or if things in this file need changing, please don't do
 # edits to this script without asking me.
 
-COMM="plot"
-COMM_EPS="plot"
-#echo $COMM
-for item in $@
+PLOTS=()
+COMM_LIST=()
+
+function help() {
+  echo "Ask Mike for help..."
+}
+
+while [ $# -gt 0 ]
 do
-  COMM="$COMM '$item' using 'ep':'re' with lines title '$item' noenhanced, '$item' using 'ep':'rol_re' with lines title '$item rolling' noenhanced, "
-  COMM_EPS="$COMM_EPS '$item' using 'ep':'epsilon' with lines title '$item' noenhanced, "
+  case "$1" in
+    -p) PLOTS+=($2); shift;;
+    -h) help; break;;
+    *) break;;
+  esac
+  shift
+done
+
+#echo "${PLOTS[@]}"
+
+IFS=";"
+
+for plt in "${PLOTS[@]}"
+do
+  read -r -a AX_LIST <<< "$plt"
+  #echo "${AX_LIST[@]}"
+  COMM="plot"
+  #echo $COMM
+  for fname in $@
+  do
+    for axis in "${AX_LIST[@]}":
+    do
+      if [ ${axis: -1} == ":" ]
+      then
+        axis=${axis::-1}
+      fi
+      COMM="$COMM '$fname' using $axis with lines title '$fname' noenhanced, "
+      #echo $COMM
+      #COMM_EPS="$COMM_EPS '$item' using 'ep':'epsilon' with lines title '$item' noenhanced, "
+    done
+  done
+  COMM_LIST+=("$COMM")
 done
 
 #echo $COMM
 
-gnuplot -p -e "set grid; set xlabel 'Epoch'; set ylabel 'Reward'; set key bmargin; $COMM"
-gnuplot -p -e "set grid; set key bmargin; $COMM_EPS"
+for __COMM in "${COMM_LIST[@]}"
+do
+  #echo $__COMM; echo
+  gnuplot -p -e "set grid; set xlabel 'Epoch'; set ylabel 'Reward'; set key bmargin; $__COMM"
+done
