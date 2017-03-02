20545ea745788e98ff0a175d51397aac37208253
Harry Shaw <netpirate99@yahoo.co.uk>
2017-03-02 23:11:10 +0000

    It sort of learns on inverted pendulum

--- MachineLearning/ActorCritic/_Actor.py
+++ MachineLearning/ActorCritic/_Actor.py
@@ -2,19 +2,20 @@ import numpy as np
 
 
 class Actor:
-    def __init__(self, state_dimensions, number_of_actions, policy_update_rate):
+    def __init__(self, state_dimensions, number_of_actions, policy_update_rate, temperature_parameter=1):
         self.number_of_actions = number_of_actions
         policy_dimensions = state_dimensions + (number_of_actions,)
         self.td_errors = np.zeros(policy_dimensions)
         self.policy = np.ones(policy_dimensions)/number_of_actions
         self.policy_update_rate = policy_update_rate
+        self.temperature_parameter = temperature_parameter
 
     def update_policy(self, state, action, td_error):
         # update TD error of action in state
         self.td_errors[state + (action,)] += self.policy_update_rate*td_error
         # recalculate policy probabilities
         state_td_errors = self.td_errors[state]
-        probabilities = softmax(state_td_errors)
+        probabilities = softmax(state_td_errors, self.temperature_parameter)
         self.policy[state] = probabilities
 
     def get_next_action(self, state):
@@ -22,9 +23,9 @@ class Actor:
         return np.random.choice(self.number_of_actions, p=probabilities)
 
 
-def softmax(state_td_errors):
+def softmax(state_td_errors, temperature_parameter):
     # scale TD errors so the mean is 0 with an absolute maximum of 2
-    normalized = state_td_errors/0.5
+    normalized = state_td_errors/temperature_parameter
 
     exponentiated = np.exp(normalized)
     sum_of_exponentiated = np.sum(exponentiated)
--- MachineLearning/ActorCritic/_ActorCriticGymTest.py
+++ MachineLearning/ActorCritic/_ActorCriticGymTest.py
@@ -2,10 +2,9 @@ import random
 
 import gym
 import numpy as np
-
-import time
-
 import ActorCritic as ac
+
+np.seterr(all='raise')
 
 # global variables
 THETAS = 20
@@ -13,32 +12,34 @@ OMEGAS = 17
 ACTIONS = 9
 
 
-def rescale(observations):
+def rescale(observations, g):
     x, y, omega_raw = observations
     theta_raw = np.arctan(x / y)
-    theta = int(round((theta_raw + np.pi) * THETAS / (2 * np.pi)))
+    theta = int(round((theta_raw + np.pi/2) * THETAS / np.pi))
     if theta == THETAS:
         theta = 0
     omega = int(round(omega_raw + 8) * (OMEGAS - 1) / 16)
+    if g is not None:
+        g.write("{}\t{}\n".format(theta, omega))
     return theta, omega
 
 
 def torque_from(action):
-    return np.array((action * 4.0 / ACTIONS - 2,))
+    return np.array((action * 4.0 / (ACTIONS - 1) - 2,))
 
 
-def get_observations(torque, env):
+def get_observations(torque, env, g=None):
     observations = env.step(torque)
     positions, reward, failed, info = observations
-    theta, omega = rescale(positions)
+    theta, omega = rescale(positions, g)
 
     return (theta, omega), reward
 
 
 # resets enviroment to random position and returns the initial position and velocity
-def reset_enviroment(env):
+def reset_enviroment(env, g=None):
     observations = env.reset()
-    theta, omega = rescale(observations)
+    theta, omega = rescale(observations, g)
     return theta, omega
 
 
@@ -46,44 +47,50 @@ def main():
     env = gym.make('Pendulum-v0')
     env.reset()
     f = open('gym.txt', 'w')
+    g = None
 
     # (positions, velocities)
     state_dimensions = (THETAS, OMEGAS)
 
     discount = 0.8
 
-    actor = ac.ActorCritic(ACTIONS, state_dimensions, discount, value_learning_rate=0.5, policy_update_rate=0.1)
+    actor_critic = ac.ActorCritic(ACTIONS, state_dimensions, discount, value_learning_rate=0.5, policy_update_rate=0.5)
 
     f.write('epoch\treward\n')
-    epochs = 10000
-    exploration = epochs / 2
-    display = 9 * epochs / 10
+    epochs = 200000
+    exploration = epochs / 4
+    display = 19 * epochs / 20
     for epoch in range(epochs):
         # reset environment and extract initial state
-        state = reset_enviroment(env)
+        state = reset_enviroment(env, g)
         cumulativeReward = 0
         print epoch
         for step in range(300):
-            if epoch > exploration:
-                action = actor.get_next_action(state)
-            else:
-                action = random.randint(0, ACTIONS-1)
+            # if epoch > exploration:
+            #     action = actor_critic.get_next_action(state)
+            # else:
+            #     action = random.randint(0, ACTIONS - 1)
+
+            action = actor_critic.get_next_action(state)
+
             torque = torque_from(action)
 
             # perform action on environment
-            new_state, reward = get_observations(torque, env)
+            new_state, reward = get_observations(torque, env, g)
             cumulativeReward += reward
 
             # critique the quality of the action
-            actor.critique(state, action, new_state, reward)
+            actor_critic.critique(state, action, new_state, reward)
             state = new_state
 
             # print display, ",", epochs
             # if epoch > display:
             #     env.render()
+        actor_critic.actor.temperature_parameter = np.abs(actor_critic.actor.td_errors).max() / 3
 
         f.write('{}\t{}\n'.format(epoch, cumulativeReward))
     f.close()
+    np.savetxt('gym_values', actor_critic.critic.values)
 
 
 if __name__ == '__main__':
