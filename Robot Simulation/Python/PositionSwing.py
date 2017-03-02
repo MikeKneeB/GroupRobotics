aaf214b3fad99c075e000f95050109efc0222d38
hargwit <harrygwithers@gmail.com>
2017-03-02 23:38:16 +0000

    Added numerical model to swing by position

---
+++ Robot Simulation/Python/PositionSwing.py
@@ -0,0 +1,44 @@
+import time
+import SwingAPI
+import SwingProxy
+
+from naoqi import ALProxy
+
+def Main():
+    #Address of Robot and Swing
+    IP = "127.0.0.1"
+    ROBOT_PORT = 9559
+    SWING_PORT = 5005
+    #Robot and Swing proxies
+    motionProxy = ALProxy("ALMotion", IP, ROBOT_PORT)
+    swingProxy = SwingProxy.SwingProxy(IP, SWING_PORT)
+
+    #Natural period of the swing
+    period = 1/2.435
+
+    #Initialise variables
+    prevAngle = 0
+    start = 0
+    toMove = False
+
+    #Loop
+    while True:
+        #Get the next angle
+        angle = swingProxy.get_angle()
+        #If negative then just passed bottom of swing
+        if (prevAngle*angle) < 0:
+            #Set time passed bottom of swing
+            start = time.time()
+            toMove = True
+        #Change position at estimated top of swing and if not in correct pos
+        if (time.time()-start) > (period/4) and toMove:
+            #Decide which position to switch to
+            if angle > 0:
+                SwingAPI.position1(motionProxy)
+                toMove = False
+            elif angle < 0:
+                SwingAPI.position2(motionProxy)
+                toMove = False
+        #Set prevAngle for next iteration
+        prevAngle = angle
+Main()
--- Robot Simulation/Python/SwingLegs.py
+++ Robot Simulation/Python/SwingLegs.py
@@ -7,8 +7,8 @@ from naoqi import ALProxy
 
 def Main():
     #IP, port and motion proxy
-    robotIP="127.0.0.1"
-    PORT=9559
-    motionProxy=ALProxy("ALMotion",robotIP,PORT)
+    robotIP = "127.0.0.1"
+    PORT = 9559
+    motionProxy = ALProxy("ALMotion",robotIP,PORT)
     SwingAPI.position2(motionProxy)
 Main()
